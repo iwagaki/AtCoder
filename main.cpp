d49d937c96c60e697cfb18fb9b5132fc5a5541de
iwagaki <iwagaki@users.noreply.github.com>
2017-08-27 21:24:16 +0900

    Added a template file

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <array>
+#include <climits> // INT, LONG, LLONG, UINT, ULONG, ULLONG
+#include <cmath>
+#include <cstdint>
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+
+#define INF (INT_MAX >> 1)
+
+using namespace std;
+
+int main() {
+
+    return 0;
+}
