10c234979f465da92392b186e7ef62f2f7ddecc9
iwagaki <iwagaki@users.noreply.github.com>
2017-09-17 21:00:53 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,20 @@
 #include <algorithm>
 #include <array>
-#include <climits> // INT, LONG, LLONG, UINT, ULONG, ULLONG
+#include <climits> // INT_MAX, LONG_MAX, LLONG_MAX, UINT_MAX, ULONG_MAX, ULLONG_MAX
 #include <cmath>
 #include <cstdint>
 #include <iostream>
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 
-#define INF (INT_MAX >> 1)
+#define INF 1e9
+#define REP(i,n) for (int i = 0; i < (int)(n); ++i)
+#define LOG(x) cout << #x << " = " << (x) << endl
 
 using namespace std;
 
 int main() {
-
     return 0;
 }
