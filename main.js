9f1c1272ebe2f135d21b87a12a54243d2f9efc05
iwagaki <iwagaki@users.noreply.github.com>
2017-10-29 17:11:13 +0900

    Update the javascript template

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
+var readLines = '';
 
 function parseInt10(elem) {
   return parseInt(elem, 10);
 }
 
 process.stdin.on('data', function (chunk) {
-  var lines = chunk.toString().split('\n');
+  readLines += chunk;
+});
+
+process.stdin.on('end', function () {
+  var lines = readLines.split('\n');
   var intArray = lines[0].trim().split(' ').map(parseInt10);
 });
