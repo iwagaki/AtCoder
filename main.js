e71d9f30f2bf1e48f47bfa84e2ca4fc62e29c07b
iwagaki <iwagaki@users.noreply.github.com>
2017-10-30 11:17:39 +0900

    Update the Javascript template

--- main.js
+++ main.js
@@ -13,4 +13,6 @@ process.stdin.on('data', function (chunk) {
 process.stdin.on('end', function () {
   var lines = readLines.split('\n');
   var intArray = lines[0].trim().split(' ').map(parseInt10);
+
+  console.log(intArray);
 });
