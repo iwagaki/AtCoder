fe4c00d9a01a6b531ee0e8ad3ad379a002d04fd6
iwagaki <iwagaki@users.noreply.github.com>
2018-01-24 02:34:19 +0900

    Updated ARC089 D - Checker

--- arc089/d/main.cpp
+++ arc089/d/main.cpp
@@ -17,39 +17,7 @@
 
 using namespace std;
 
-
-int countmap[2][2000][2000];
-
-
-int getScore(int i, int x, int y, int k) {
-    if (x < 0 || y < 0 || x - k + 1 >= 2 * k || y - k + 1 >= 2 * k)
-        return 0;
-
-    int x1 = min(x - k, 2 * k - 1);
-    int y1 = min(y, 2 * k - 1);
-
-    int x2 = min(x, 2 * k - 1);
-    int y2 = min(y - k, 2 * k - 1);
-
-    int x3 = min(x - k, 2 * k - 1);
-    int y3 = min(y - k, 2 * k - 1);
-
-    x = min(x, 2 * k - 1);
-    y = min(y, 2 * k - 1);
-
-    int score = countmap[i][x][y];
-
-    if (x1 >= 0 && y1 >= 0)
-        score -= countmap[i][x1][y1];
-
-    if (x2 >= 0 && y2 >= 0)
-        score -= countmap[i][x2][y2];
-
-    if (x3 >= 0 && y3 >= 0)
-        score += countmap[i][x3][y3];
-
-    return score;
-}
+int s[2][1000][1000];
 
 
 int main() {
@@ -60,33 +28,35 @@ int main() {
         int xi, yi;
         string ci;
         cin >> xi >> yi >> ci;
-        countmap[(ci[0] == 'W') ? 0 : 1][xi % (2 * k)][yi % (2 * k)]++;
+
+        if (ci[0] == 'B')
+            xi += k; // Do not use -= for %
+        if (xi % (2 * k) >= k) {
+            xi += k; // Do not use -= for %
+            yi += k; // Do not use -= for %
+        }
+
+        if (yi % (2 * k) >= k)
+            s[1][xi % k][yi % k]++;
+        else
+            s[0][xi % k][yi % k]++;
     }
 
-    REP(p, 2) REP(q, 2 * k) REP(r, 2 * k) {
-        if (q > 0)
-            countmap[p][q][r] += countmap[p][q - 1][r];
-        if (r > 0)
-            countmap[p][q][r] += countmap[p][q][r - 1];
-        if (q > 0 && r > 0)
-            countmap[p][q][r] -= countmap[p][q - 1][r - 1];
+    REP(p, 2) {
+        for (int q = 1; q < k; q++) s[p][q][0] += s[p][q - 1][0];
+        for (int r = 1; r < k; r++) s[p][0][r] += s[p][0][r - 1];
+        for (int q = 1; q < k; q++) for (int r = 1; r < k; r++) s[p][q][r] += s[p][q - 1][r] + s[p][q][r - 1] - s[p][q - 1][r - 1];
     }
 
     int best = 0;
 
-    REP(p, 2 * k) REP(q, 2 * k) {
-        int current = 0;
-
-        int index = 0;
-        REP(i, 5) REP(j, 5) {
-            current += getScore(index, p - 2 * k + k * i, q - 2 * k + k * j, k);
-            index ^= 1;
-        }
-
-        best = max(best,current);
+    REP(p, k) REP(q, k) {
+        best = max(best,
+                   max(s[0][k - 1][k - 1] - s[0][p][k - 1] - s[0][k - 1][q] + 2 * s[0][p][q] + s[1][p][k - 1] + s[1][k - 1][q] - 2 * s[1][p][q],
+                       s[1][k - 1][k - 1] - s[1][p][k - 1] - s[1][k - 1][q] + 2 * s[1][p][q] + s[0][p][k - 1] + s[0][k - 1][q] - 2 * s[0][p][q]));
     }
-    
+
     cout << best << endl;
-    
+
     return 0;
 }
